1bb87b9bb1f5c2a2bd4efab76f243af507caa4db
A Ahmad <57046949+abbaahmad@users.noreply.github.com>
2022-03-30 23:11:16 +0300

    Add files via upload

---
+++ FilmprojectApplication.java
@@ -0,0 +1,13 @@
+package com.flexisaf.fip.filmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmprojectApplication.class, args);
+	}
+
+}
