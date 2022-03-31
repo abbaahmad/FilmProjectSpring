abd1f347548642fa45fe60f197a8d54a37224b73
A Ahmad <57046949+abbaahmad@users.noreply.github.com>
2022-03-31 10:32:36 +0100

    Create FilmRepository.java

---
+++ src/main/repositories/FilmRepository.java
@@ -0,0 +1,15 @@
+package com.flexisaf.fip.filmproject.repositories;
+
+import com.flexisaf.fip.filmproject.models.Film;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FilmRepository extends JpaRepository<Film, Long> {
+
+    @Query("SELECT f FROM Film f WHERE f.name = ?1")
+    Optional<Film> findFilmByName(String name);
+}
